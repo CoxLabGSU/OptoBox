bab35aa145a79b7010ae6d71a87aa0e3fcd4d18f
kjdgsu <42543585+kjdgsu@users.noreply.github.com>
2022-02-16 10:26:05 -0500

    Create OptoBox5secOn5minOff.ino
    Initial commit of control software, pre-STAR Protocols submission

---
+++ OptoBox5secOn5minOff.ino
@@ -0,0 +1,37 @@
+// Copyright 2022 Kevin J. Donaldson
+
+void setup() {
+  // Stimulus Parameters: 5 seconds on, every 5 minutes (modeled after Ye Lab 2017 Neuron paper)
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+int State = 0;
+pinMode(3,OUTPUT); // initiate Channel 3
+pinMode(5,OUTPUT); // initiate Channel 5
+pinMode(6,OUTPUT); // initiate Channel 6
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+// 1st group  
+digitalWrite(3,HIGH); //turn on LED Group 1 by passing 5V through pin 3, specified by HIGH
+Serial.println("LED Group 1 on for 5 seconds");
+
+digitalWrite(5,HIGH); //turn on LED Group 2 by passing 5V through pin 5, specified by HIGH
+Serial.println("LED Group 2 on for 5 seconds");
+
+digitalWrite(6,HIGH); //turn on LED by passing 5V through pin 6, specified by HIGH
+Serial.println("LED Group 3 Lights on for 5 seconds");
+
+//Keep LEDs on for 5 seconds (units=milliseconds)
+delay(5000);
+
+//Turn off LEDs
+digitalWrite(3,LOW); //turn off LED Group 3, specified by LOW
+digitalWrite(5,LOW); //turn off LED Group 5, specified by LOW
+digitalWrite(6,LOW); //turn off LED Group 6, specified by LOW
+
+//Stay off for 5 minutes
+delay(30000); // stay in this state for 5 minutes
+Serial.println("ALL LEDs should be off, begin 5 min delay");
+}
